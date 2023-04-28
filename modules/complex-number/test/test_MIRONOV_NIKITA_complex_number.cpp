32960e703cab85c281bb80e7461f7f243e8466d0
onkelzeddy <neckit.nic@yandex.ru>
2023-04-28 12:59:33 +0300

    Style fix

--- modules/complex-number/test/test_MIRONOV_NIKITA_complex_number.cpp
+++ modules/complex-number/test/test_MIRONOV_NIKITA_complex_number.cpp
@@ -6,11 +6,11 @@
 
 TEST(Mironov_Nikita_ComplexNumberTest, zero_args_constructor_returns_zero) {
     // Arrange
-    ComplexNumber a;
+    ComplexNumber zeroComplexNumber;
 
     // Assert
-    EXPECT_DOUBLE_EQ(a.getRe(), 0.0);
-    EXPECT_DOUBLE_EQ(a.getIm(), 0.0);
+    EXPECT_DOUBLE_EQ(zeroComplexNumber.getRe(), 0.0);
+    EXPECT_DOUBLE_EQ(zeroComplexNumber.getIm(), 0.0);
 }
 
 TEST(Mironov_Nikita_ComplexNumberTest, constructor_with_args_works) {
@@ -23,19 +23,20 @@ TEST(Mironov_Nikita_ComplexNumberTest, constructor_with_args_works) {
 
 TEST(Mironov_Nikita_ComplexNumberTest, divide_by_zero_is_prohibited) {
     // Arrange
-    ComplexNumber a(2.0, 3.1);
-    ComplexNumber b(0.0, 0.0);
+    ComplexNumber complexDivisible(2.0, 3.1);
+    ComplexNumber zeroComplexDivider(0.0, 0.0);
     ComplexNumber result;
 
     // Assert
-    ASSERT_ANY_THROW(result = a / b);
+    ASSERT_ANY_THROW(result = complexDivisible / zeroComplexDivider);
 }
 
 TEST(Mironov_Nikita_ComplexNumberTest, sum_works) {
     // Arrange
-    ComplexNumber a(3.0, 3.1);
+    ComplexNumber first(3.0, 3.1);
     ComplexNumber b(2.0, 1.3);
-    ComplexNumber result, expect(5.0, 4.4);
+    ComplexNumber result;
+    ComplexNumber expect(5.0, 4.4);
 
     // Act
     result = a + b;
@@ -47,12 +48,13 @@ TEST(Mironov_Nikita_ComplexNumberTest, sum_works) {
 
 TEST(Mironov_Nikita_ComplexNumberTest, sub_works) {
     // Arrange
-    ComplexNumber a(2.0, 3.1);
-    ComplexNumber b(3.0, 1.5);
-    ComplexNumber result, expect(-1.0, 1.6);
+    ComplexNumber complexReduced(2.0, 3.1);
+    ComplexNumber complexDeductible(3.0, 1.5);
+    ComplexNumber result;
+    ComplexNumber expect(-1.0, 1.6);
 
     // Act
-    result = a - b;
+    result = complexReduced - complexDeductible;
 
     // Assert
     ASSERT_EQ(expect.getIm(), result.getIm());
@@ -61,12 +63,13 @@ TEST(Mironov_Nikita_ComplexNumberTest, sub_works) {
 
 TEST(Mironov_Nikita_ComplexNumberTest, mul_works) {
     // Arrange
-    ComplexNumber a(6.3, 2.1);
-    ComplexNumber b(1.1, 1.3);
-    ComplexNumber result, expect(4.2, 10.5);
+    ComplexNumber complexMultiplied(6.3, 2.1);
+    ComplexNumber complexMultiplier(1.1, 1.3);
+    ComplexNumber result;
+    ComplexNumber expect(4.2, 10.5);
 
     // Act
-    result = a * b;
+    result = complexMultiplied * complexMultiplier;
 
     // Assert
     ASSERT_EQ(expect.getIm(), result.getIm());
@@ -75,12 +78,13 @@ TEST(Mironov_Nikita_ComplexNumberTest, mul_works) {
 
 TEST(Mironov_Nikita_ComplexNumberTest, div_works) {
     // Arrange
-    ComplexNumber a(-2.0, 1.0);
-    ComplexNumber b(1.0, -1.0);
-    ComplexNumber result, expect(-1.5, -0.5);
+    ComplexNumber complexDivisible(-2.0, 1.0);
+    ComplexNumber complexDivider(1.0, -1.0);
+    ComplexNumber result;
+    ComplexNumber expect(-1.5, -0.5);
 
     // Act
-    result = a / b;
+    result = complexDivisible / complexDivider;
 
     // Assert
     ASSERT_EQ(expect.getIm(), result.getIm());
@@ -90,18 +94,18 @@ TEST(Mironov_Nikita_ComplexNumberTest, div_works) {
 TEST(Mironov_Nikita_ComplexNumberTest, equality_works) {
     // Arrange
     double re = 2.0, im = -1.0;
-    ComplexNumber a(re, im);
-    ComplexNumber b(re, im);
+    ComplexNumber firstOfEqual(re, im);
+    ComplexNumber secondOfEqual(re, im);
 
     // Assert & Act
-    EXPECT_TRUE(a == b);
+    EXPECT_TRUE(firstOfEqual == secondOfEqual);
 }
 
 TEST(Mironov_Nikita_ComplexNumberTest, inequality_works) {
     // Arrange
-    ComplexNumber a(3.0, 2.0);
-    ComplexNumber b(2.0, -1.0);
+    ComplexNumber firstOfInequal(3.0, 2.0);
+    ComplexNumber secondOfInequal(2.0, -1.0);
 
     // Assert & Act
-    EXPECT_TRUE(a != b);
+    EXPECT_TRUE(firstOfInequal != secondOfInequal);
 }
