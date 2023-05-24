7ad82f586af8a8aa7fe2e0ed26865db850006147
onkelzeddy <onkelzeddy@users.noreply.github.com>
2023-05-24 22:47:38 +0300

    Add code

---
+++ modules/interpolation_search/include/interpolation-search.h
@@ -0,0 +1,24 @@
+// Copyright 2023 Mironov Nikita
+
+#ifndef MODULES_INTERPOLATION_SEARCH_INCLUDE_INTERPOLATION_SEARCH_H_
+#define MODULES_INTERPOLATION_SEARCH_INCLUDE_INTERPOLATION_SEARCH_H_
+
+#include <vector>
+
+class interpolationSearch {
+    public:
+        interpolationSearch(const std::vector<int>);
+        void set_vector(const std::vector<int>);
+        int search(const int);
+        ~interpolationSearch();
+
+    private:
+        std::vector<int> original_target_vector;
+        std::vector<int> target_vector;
+        int vector_size;
+        void sort_target_vector();
+        bool target_vector_is_sorted();
+};
+
+
+#endif // MODULES_INTERPOLATION_SEARCH_INCLUDE_INTERPOLATION_SEARCH_H_
--- modules/interpolation_search/test/test_interpolation_search.cpp
+++ modules/interpolation_search/test/test_interpolation_search.cpp
@@ -1,1 +1,90 @@
 // Copyright 2023 Mironov Nikita
+#include <gtest/gtest.h>
+
+#include "include/interpolation-search.h"
+
+TEST(Iterpolation_search_test, Class_can_be_created) {
+    std::vector<int> test_vector = {1, 3, 5, 8};
+    ASSERT_NO_THROW(interpolationSearch is(test_vector));
+}
+
+TEST(Iterpolation_search_test, Class_throw_error_on_empty_vector) {
+    std::vector<int> test_vector;
+    ASSERT_ANY_THROW(interpolationSearch is(test_vector));
+}
+
+TEST(Iterpolation_search_test, You_can_set_new_vector) {
+    std::vector<int> test_vector = {1, 3, 5, 8};
+    interpolationSearch is(test_vector);
+
+    test_vector = {1, 2, 1, 9};
+    ASSERT_NO_THROW(is.set_vector(test_vector));
+}
+
+TEST(Iterpolation_search_test, You_can_not_set_new_empty_vector) {
+    std::vector<int> test_vector = {1, 3, 5, 8};
+    interpolationSearch is(test_vector);
+
+    std::vector<int> empty_test_vector;
+    ASSERT_NO_THROW(is.set_vector(empty_test_vector));
+}
+
+TEST(Iterpolation_search_test, Search_in_sorted_vector) {
+    std::vector<int> test_vector = {1, 3, 5, 8, 9};
+    interpolationSearch is(test_vector);
+
+    int search_target = 6;
+    int expected_search_result = -1;
+
+    ASSERT_EQ(expected_search_result, is.search(search_target));
+}
+
+TEST(Iterpolation_search_test, Search_in_unsorted_vector) {
+    std::vector<int> test_vector = {9, 1, 6, 2, 1, 7};
+    interpolationSearch is(test_vector);
+
+    int search_target = 5;
+    int expected_search_result = -1;
+
+    ASSERT_EQ(expected_search_result, is.search(search_target));
+}
+
+TEST(Iterpolation_search_test, Successful_search_in_sorted_vector) {
+    std::vector<int> test_vector = {1, 2, 7, 12, 25, 27};
+    interpolationSearch is(test_vector);
+
+    int search_target = 2;
+    int expected_search_result = 1;
+
+    ASSERT_EQ(expected_search_result, is.search(search_target));
+}
+
+TEST(Iterpolation_search_test, Successful_search_in_unsorted_vector) {
+    std::vector<int> test_vector = {9, 1, 6, 2, 1, 7};
+    interpolationSearch is(test_vector);
+
+    int search_target = 2;
+    int expected_search_result = 2;
+
+    ASSERT_EQ(expected_search_result, is.search(search_target));
+}
+
+TEST(Iterpolation_search_test, Search_in_one_element_vector) {
+    std::vector<int> test_vector = {9};
+    interpolationSearch is(test_vector);
+
+    int search_target = 2;
+    int expected_search_result = -1;
+
+    ASSERT_EQ(expected_search_result, is.search(search_target));
+}
+
+TEST(Iterpolation_search_test, Successful_search_in_one_element_vector) {
+    std::vector<int> test_vector = {6};
+    interpolationSearch is(test_vector);
+
+    int search_target = 6;
+    int expected_search_result = 0;
+
+    ASSERT_EQ(expected_search_result, is.search(search_target));
+}
